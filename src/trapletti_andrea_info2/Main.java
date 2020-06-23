13acb013e63d4e988e772aeeac1e8d402e5079d8
AndreaTrapletti <67322071+AndreaTrapletti@users.noreply.github.com>
2020-06-23 14:21:08 +0200

    prima prova

---
+++ src/trapletti_andrea_info2/Main.java
@@ -0,0 +1,10 @@
+package trapletti_andrea_info2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ciao sono su github");
+	}
+
+}
